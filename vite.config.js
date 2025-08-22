8396e3b92cb7c00931e479c0740c2c01fb61e220
nikhil <nikhilakhil63@gmail.com>
2025-08-22 23:53:48 +0530

    chore: prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Health_mate_AI/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
